f42c44a0dc0a4bb73add46f0540a08245a445906
Serhat Topaloğlu <serhattopaloglu@gmail.com>
2025-09-05 18:35:17 +0300

    Enable app directory for all pages

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-  },
-  compress: true,
-  poweredByHeader: false,
-  reactStrictMode: true,
-  swcMinify: true,
+  experimental: {
+    appDir: true
+  }
 }
 
 module.exports = nextConfig
