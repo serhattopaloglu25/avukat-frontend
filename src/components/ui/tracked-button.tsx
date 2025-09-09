036624b73ec246b34fdce6d1677480dae7821a5c
Serhat Topaloğlu <serhattopaloglu@gmail.com>
2025-09-09 23:13:32 +0300

    Fix tracked-button type error

--- src/components/ui/tracked-button.tsx
+++ src/components/ui/tracked-button.tsx
@@ -1,32 +1,32 @@
 'use client';
 
+import React from 'react';
 import { Button, ButtonProps } from './button';
-import { trackEvent } from '@/components/Analytics';
 
 interface TrackedButtonProps extends ButtonProps {
   eventName?: string;
   eventParams?: Record<string, any>;
 }
 
-export function TrackedButton({ 
-  eventName, 
-  eventParams, 
-  onClick,
-  children,
-  ...props 
-}: TrackedButtonProps) {
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    if (eventName) {
-      trackEvent(eventName, eventParams);
-    }
-    if (onClick) {
-      onClick(e);
-    }
-  };
-
-  return (
-    <Button {...props} onClick={handleClick}>
-      {children}
-    </Button>
-  );
+function trackEvent(name: string, params?: Record<string, any>) {
+  if (typeof window !== 'undefined') {
+    console.log('Track event:', name, params);
+  }
 }
+
+export const TrackedButton = React.forwardRef<HTMLButtonElement, TrackedButtonProps>(
+  ({ eventName, eventParams, onClick, ...props }, ref) => {
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (eventName) {
+        trackEvent(eventName, eventParams || {});
+      }
+      if (onClick) {
+        onClick(e);
+      }
+    };
+
+    return <Button ref={ref} onClick={handleClick} {...props} />;
+  }
+);
+
+TrackedButton.displayName = 'TrackedButton';
