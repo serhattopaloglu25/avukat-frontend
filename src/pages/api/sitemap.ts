c867be2f0ab6d4533d4252034a9a706b894a5f39
Serhat Topaloğlu <serhattopaloglu@gmail.com>
2025-09-09 09:19:01 +0300

    Remove conflicting Pages API routes

--- src/pages/api/sitemap.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { generateSitemap } from '@/lib/sitemap';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader('Content-Type', 'text/xml');
-  res.status(200).send(generateSitemap());
-}
